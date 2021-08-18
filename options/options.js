078d95b1d3dc7060dde245d19be8075792bfcf7b
Crowdbotics <team@crowdbotics.com>
2021-08-18 10:37:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cookieclickers_29907Identifier",
+  url: "https://cookieclickers_29907Identifier.botics.co",
+  api: "https://cookieclickers_29907Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
